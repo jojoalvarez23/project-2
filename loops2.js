f887eeddb7636afc11454a43e640d6eac7359053
jojoalvarez23 <j.alvarezmartinez20@icloud.com>
2024-11-13 20:04:21 -0600

    first commit

---
+++ loops2.js
@@ -0,0 +1,22 @@
+test('test')
+
+var listItems = document.getElementsByTagName('li');                   // All <li> elements
+
+// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+var heading = document.querySelector('h2');                        // h2 element
+var headingText = heading.firstChild.nodeValue;                    // h2 text
+var totalItems = listItems.length;                                 // No. of <li> elements
+var newHeading =  headingText + '<span>' + totalItems + '</span>'; // Content
+
+
+/*  WRITE YOUR CODE BELOW */
+
+
+// Part1. Hint: Use the listItems object to iterate through the elements of the list
+
+
+
+
+
+
+// Part2. Hint: Use the heading and newHeading to show the number of items in the shopping list
